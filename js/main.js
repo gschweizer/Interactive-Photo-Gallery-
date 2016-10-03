5f55d6d2d58345752c63927b77bddc52b0ff6fb4
gschweizer <ginamschweizer@gmail.com>
2016-10-03 13:29:16 -0400

    Updated CSS and JS

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
+//live search
+
 $(document).ready(function(){
     $("#searchBar").keyup(function(){
  
         // Retrieve the input field text
         var filter = $(this).val();
-        var regex = new RegExp(filter, "i");   
+        var regex = new RegExp(filter, "i");
      
         // Loop through the captions
-        $("#lightGallery img").each(function() {
-          var altText = $(this).attr("alt");
+         $("#lightGallery img").each(function(){
+            var alt = $(this).attr("alt");
  
             // If the caption does not contain the text phrase fade it out
-            if ($(this).text().search(regex) < 0) {
+            if (alt.search(regex) < 0) {
                 $(this).fadeOut();
  
             // Show the image if the phrase matches
